6282f3736e6d8f9ab4ffc17bd5725f917e59ecd3
Tomas Vavrda <tomas.vavrda@advantages.cz>
2022-09-25 23:21:06 +0200

    ~ Industry section turned into accordion on mobile

--- _includes/scripts/components/industry.js
+++ _includes/scripts/components/industry.js
@@ -3,28 +3,19 @@ const industry = () => {
 
 	if ($industry) {
 		var $buttons = $industry.querySelectorAll(".links ul li");
-		var $images = $industry.querySelectorAll(".images ul li");
 	}
 
 	function clearSelected() {
 		$buttons.forEach(($button) => {
 			$button.classList.remove("selected");
 		});
-
-		$images.forEach(($image) => {
-			$image.classList.remove("selected");
-		});
 	}
 
 	function init() {
 		$buttons.forEach(($btn, $i) => {
 			$btn.addEventListener("click", function ($e) {
-				$e.preventDefault();
-
-				if (window.innerWidth <= 928) {
-					$industry
-						.querySelector(".images")
-						.scrollIntoView({ block: "start", behavior: "smooth" });
+				if (window.innerWidth > 928) {
+					$e.preventDefault();
 				}
 
 				// Clear selected
@@ -32,7 +23,6 @@ const industry = () => {
 
 				// Set selected
 				$btn.classList.toggle("selected");
-				$images[$i].classList.toggle("selected");
 			});
 		});
 	}
